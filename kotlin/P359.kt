bd36c9db02a7c1f28c0320be99936f65dd5162b9
Guanhao (Daniel) Lin <ghlin.daniel@gmail.com>
2026-05-10 23:22:07 -0700

    P359

---
+++ kotlin/P359.kt
@@ -0,0 +1,16 @@
+class P359 {
+  class Logger() {
+
+    private val map = mutableMapOf<String, Int>()
+
+    /** Time: O(1), Space: O(n) for distinct messages stored in the map */
+    fun shouldPrintMessage(timestamp: Int, message: String): Boolean {
+      return if (map[message]?.let { timestamp < it + 10 } == true) {
+        false
+      } else {
+        map[message] = timestamp
+        true
+      }
+    }
+  }
+}
